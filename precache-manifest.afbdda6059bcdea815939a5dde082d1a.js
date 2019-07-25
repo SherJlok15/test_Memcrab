b2e4ce5b2f47f06e6ea6b28bb347237edccfb85f
Fandorin <SherJlok15@gmail.com>
2019-07-25 12:08:50 +0300

    Updates

---
+++ precache-manifest.afbdda6059bcdea815939a5dde082d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31964c980ad06386bdb36feef11b9476",
+    "url": "/test_Memcrab/index.html"
+  },
+  {
+    "revision": "e4d8ce8de81093e8cb29",
+    "url": "/test_Memcrab/static/css/main.0f207e4d.chunk.css"
+  },
+  {
+    "revision": "cb8866087bd75547b005",
+    "url": "/test_Memcrab/static/js/2.c4dea77d.chunk.js"
+  },
+  {
+    "revision": "e4d8ce8de81093e8cb29",
+    "url": "/test_Memcrab/static/js/main.27739830.chunk.js"
+  },
+  {
+    "revision": "7881a86c6d718afb3d03",
+    "url": "/test_Memcrab/static/js/runtime~main.c1048ab6.js"
+  }
+]);
